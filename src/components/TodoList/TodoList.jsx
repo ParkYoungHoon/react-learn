efd13d960d0cb7c8fc943d4874a4d39e7b19393c
Tavi <secret9179@gmail.com>
2022-11-15 06:09:36 +0900

    delete todo

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
+import Todo from "../Todo/Todo";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
-    { id: "1", text: "독서", status: "active" },
-    { id: "2", text: "영어공부", status: "active" },
+    { id: "123", text: "장보기", status: "active" },
+    { id: "124", text: "공부하기", status: "active" },
   ]);
-
-  const handleAddTodo = (todo) => setTodos([...todos, todo]);
-
+  const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((t) => t.id !== deleted.id));
   return (
     <section>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+        {todos.map((item) => (
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
         ))}
       </ul>
-      <AddTodo onAdd={handleAddTodo} />
+      <AddTodo onAdd={handleAdd} />
     </section>
   );
 }
